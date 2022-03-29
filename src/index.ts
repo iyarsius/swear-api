5bd9169e3b061547373f2ed1bcd83ce19680fff6
iyarsius <iyarsius@gmail.com>
2022-03-29 16:00:14 +0200

    Removed BaseScraper extension from main class
    Main class has been renamed SwearApi and the baseScraper extension has been removed for a better lisibility in types

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-import { BaseScraper } from "./scrapers/BaseScraper";
 import { IScraperConfig } from "./typing/BaseScraper";
 import { Stockx } from "./scrapers/stockx";
 import { Goat } from "./scrapers/goat";
 
-export class HypeScraper extends BaseScraper {
+export class SwearApi {
     public stockx: Stockx;
     public goat: Goat;
 
-    constructor(config: IScraperConfig) {     
-        super(config);
+    constructor(config: IScraperConfig) {
         this.stockx = new Stockx(config);
         this.goat = new Goat(config);
     };
